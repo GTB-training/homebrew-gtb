8fce585bb296275f47a4648a4d447d96662ac33b
Xiaofeng Wang <xfun68@gmail.com>
2020-08-24 15:53:25 +0800

    remove generated comments

--- util.rb
+++ util.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Util < Formula
   desc "Utilities to help download students homework repositories, etc."
   homepage ""
