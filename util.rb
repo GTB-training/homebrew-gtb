170ba985755dd5b5a2f6f464d8cce5aaea1470ab
Xiaofeng Wang <xfun68@gmail.com>
2020-08-24 12:04:14 +0800

    add util.rb formula

---
+++ util.rb
@@ -0,0 +1,14 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Util < Formula
+  desc "Utilities to help download students homework repositories, etc."
+  homepage ""
+  url "https://github.com/GTB-training/util/archive/v1.0.0.tar.gz"
+  sha256 "ad12ad6e15f6e377d874596fd3bc5d43f73ac10a8d999af3559e341704c5670b"
+
+  def install
+     bin.install "gtb"
+  end
+
+end
