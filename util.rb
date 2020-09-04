90eb245da51cf94699642004522fde177607dce7
Xiaofeng Wang <xfun68@gmail.com>
2020-09-04 14:58:08 +0800

    upgrade to v1.1.1

--- util.rb
+++ util.rb
@@ -1,8 +1,8 @@
 class Util < Formula
   desc "Utilities to help download students homework repositories, etc."
   homepage ""
-  url "https://github.com/GTB-training/util/archive/v1.1.0.tar.gz"
-  sha256 "6d1d1189907cf88ad738d31a53a94f968a68614acca9f0bf3f746c450c9a87f0"
+  url "https://github.com/GTB-training/util/archive/v1.1.1.tar.gz"
+  sha256 "6b92788181bb755a2b3361ada78445b34262e5f35d8ba0ec3c08baa2c87e3141"
 
   def install
      bin.install "gtb"
