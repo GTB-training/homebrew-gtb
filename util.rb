cfe967bc9ca6e9f28b4b185024a314a1b8c5b521
Xiaofeng Wang <xfun68@gmail.com>
2020-10-21 19:44:31 +0800

    upgrade to v1.5.3

--- util.rb
+++ util.rb
@@ -1,8 +1,8 @@
 class Util < Formula
   desc "Utilities to help download students homework repositories, etc."
   homepage ""
-  url "https://github.com/GTB-training/util/archive/v1.5.2.tar.gz"
-  sha256 "c3abc3f0918a5910699e1601dfa5830d43224659b682d65b52763af931586730"
+  url "https://github.com/GTB-training/util/archive/v1.5.3.tar.gz"
+  sha256 "dce78d27ba31f85058c4f92e5b97cf30cdee28567d1b3d531751191d35f1c6f6"
 
   depends_on "gh"
   depends_on "jsonpp"
