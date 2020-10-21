dac2ddc447a5faa956fbb751ac24e42740e7dc13
Xiaofeng Wang <xfun68@gmail.com>
2020-10-21 20:01:46 +0800

    change download url

--- util.rb
+++ util.rb
@@ -1,7 +1,7 @@
 class Util < Formula
   desc "Utilities to help download students homework repositories, etc."
   homepage ""
-  url "https://github.com/GTB-training/util/archive/v1.5.3.tar.gz"
+  url "https://codeload.github.com/GTB-training/util/tar.gz/1.5.3"
   sha256 "dce78d27ba31f85058c4f92e5b97cf30cdee28567d1b3d531751191d35f1c6f6"
 
   depends_on "gh"
