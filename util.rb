18f909bd2b912f73628ee004f43a91b2e99f64a4
Xiaofeng Wang <xfun68@gmail.com>
2020-10-29 20:06:23 +0800

    fix broken url

--- util.rb
+++ util.rb
@@ -1,7 +1,7 @@
 class Util < Formula
   desc "Utilities to help download students homework repositories, etc."
   homepage ""
-  url "https://codeload.github.com/GTB-training/util/tar.gz/1.8.0"
+  url "https://codeload.github.com/GTB-training/util/tar.gz/v1.8.0"
   sha256 "3823e510ef081e6832cf46e69ba31aaed5c53ebabb7ec73ecc0bffcedd3dd4ad"
 
   depends_on "gh"
