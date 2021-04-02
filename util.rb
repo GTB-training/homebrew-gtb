988de1445614eb9562f8c1595c0b0bc405d84fa6
Xiaofeng Wang <xfun68@gmail.com>
2021-04-02 18:48:09 +0800

    upgrade to v1.52.0

--- util.rb
+++ util.rb
@@ -1,8 +1,8 @@
 class Util < Formula
   desc "Utilities to help download students homework repositories, etc."
   homepage ""
-  url "https://codeload.github.com/GTB-training/util/tar.gz/v1.51.0"
-  sha256 "c847c50a977051c33c7e73fd028bf4bb1f2b50ed4917008b725059cca88cb780"
+  url "https://codeload.github.com/GTB-training/util/tar.gz/v1.52.0"
+  sha256 "f8d4bc29bdc1faa38ffbf9aa9878d785e812084860b5e5535c81e7d8a247d10a"
 
   depends_on "gh"
   depends_on "pv"
