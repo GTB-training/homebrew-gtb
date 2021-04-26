bdffbb17ebdf893db29148261208c030a6b2505e
Xiaofeng Wang <xfun68@gmail.com>
2021-04-26 15:36:58 +0800

    chore: specify min version of dependencies

--- util.rb
+++ util.rb
@@ -4,13 +4,13 @@ class Util < Formula
   url "https://codeload.github.com/GTB-training/util/tar.gz/v1.79.0"
   sha256 "81d9a62d8ea3d5e5e9f8fe3fefb12b840e58a1acf9e9c66867a26472b6424e24"
 
-  depends_on "gh"
-  depends_on "pv"
-  depends_on "node"
-  depends_on "httpie"
-  depends_on "jsonpp"
+  depends_on "gh" => "1.9.2"
+  depends_on "pv" => "1.6.6"
+  depends_on "node" => "16.0.0"
+  depends_on "httpie" => "2.4.0"
+  depends_on "jsonpp" => "1.3.0"
   depends_on "parallel"
-  depends_on "datamash"
+  depends_on "datamash" => "1.7"
 
   def install
      bin.install "gtb"
