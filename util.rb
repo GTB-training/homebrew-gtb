48e4d4f508d08c42e82790c58dabdc8dcb346494
gitqh <quhang19881208@163.com>
2022-11-11 17:23:36 +0800

    upgrade to v1.264.1

--- util.rb
+++ util.rb
@@ -1,8 +1,8 @@
 class Util < Formula
   desc "Utilities to help download students homework repositories, etc."
   homepage ""
-  url "https://codeload.github.com/GTB-training/util/tar.gz/v1.263.3"
-  sha256 "06d5bc340412513f7e7739b935ac94f8825c7cd5f37f3ca51f5207d100d4a6e4"
+  url "https://codeload.github.com/GTB-training/util/tar.gz/v1.264.1"
+  sha256 "669f8155f4ec1a53727f39d1ff99cf23edc7abfa756810c41cdd989b3adaf94e"
 
   depends_on "gh" => "1.9.2"
   depends_on "pv" => "1.6.6"
